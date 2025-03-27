1fde03971207ff467422ab3b6b490e0a4862aa9b
Kevin Loughead <kvnloughead@gmail.com>
2025-03-26 23:01:06 -0400

    feat: add method to draw line between cells

--- cell.py
+++ cell.py
@@ -7,10 +7,14 @@ class Cell:
         self.has_top_wall = True 
         self.has_right_wall = True
         self.has_bottom_wall = True
+
         self._upper_left = p1
         self._upper_right = Point(p1.x, p2.y)
         self._lower_right = p2
         self._lower_left = Point(p2.x, p1.y)
+        self._center = Point(min(p1.x, p2.x) + abs((p1.x - p2.x) / 2),
+                             min(p1.x, p2.x) + abs((p1.y - p2.y) / 2))
+
         self._window = window
 
     def draw(self, fill_color="black"):
@@ -22,4 +26,9 @@ class Cell:
         draw_wall(self.has_top_wall, self._upper_left, self._upper_right)
         draw_wall(self.has_right_wall, self._upper_right, self._lower_right)
         draw_wall(self.has_bottom_wall, self._lower_left, self._lower_right)
-        
+
+    def draw_move(self, to_cell, undo=False, fill_color="grey"):
+        """Draws a path from the center of the current cell to the center of to_cell."""
+        if undo:
+            fill_color = "red"
+        self._window.draw_line(Line(self._center, to_cell._center), fill_color)
